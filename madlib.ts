750c691d6e05eefdbedb500e52b6003293dfa01b
NateTheUnavailable <nate@stevenogardner.com>
2024-11-21 11:41:09 -0700

    started program

---
+++ madlib.ts
@@ -0,0 +1,40 @@
+import * as promptSync from 'prompt-sync';
+const prompt = promptSync();
+let noun = "";
+let noun2 = "";
+let noun3 = "";
+let noun4 = "";
+let noun5 = "";
+let adj = "";
+let adj1 = "";
+let adj2 = "";
+let adj3 = "";
+let adj4 = "";
+let pnoun = "";
+let verb = "";
+let number = "";
+console.log('Please give me a noun');
+noun = prompt("noun: ")
+console.log('Please give me another noun');
+noun = prompt("noun 2: ")
+console.log('Please give me another noun');
+noun = prompt('noun 3: ')
+console.log('Please give me another noun');
+noun = prompt('noun 4: ')
+console.log('Please give me another noun');
+noun = prompt('noun 5: ')
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
+
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
+
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
+
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
+
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
+
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
+
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
+
+console.log('Today was my first day of grade. My teacher is ms.. She seems and');
